42c84317f15aade2161a2f06cade9b5a2a758ad9
maevedoa <mlittle7823@floridapoly.edu>
2025-08-20 18:42:11 +0000

    bleep

---
+++ first_program.cpp
@@ -0,0 +1,16 @@
+/*
+08/20/2025
+First program OOP
+*/
+#include <iostream>
+
+// create main function
+int main(void) {
+    std::cout << "woof woof im a doggy" << std::endl;
+    // std - standard namespace (feature that helps organize code, like a folder)
+    // :: - scope resolution operator
+    // cout - console output (object of an iostream class) (create object, pass data to object, prints info)
+    // << - stream insertion operator (sends to the object for it to print)
+    // endl - end of line (or new one)
+    return 0;
+}
