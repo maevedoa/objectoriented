728487679f13bbb346723afb26cdadb03fc524b0
maevedoa <mlittle7823@floridapoly.edu>
2025-09-15 18:40:21 +0000

    fixed + test function bc why not

--- p1_lab3.cpp
+++ p1_lab3.cpp
@@ -7,7 +7,7 @@ int numberOfDuplicates(const vector<int>& nums) {
     unordered_map<int, int> count;
 
     for (int num : nums) {
-        count[nums]++; 
+        count[num]++; 
     }
     int duplicateCount = 0;
     for (const auto& pair : count) {
@@ -22,9 +22,18 @@ void printVector(const vector<int>& nums) {
     cout << "{ ";
     for(size_t i = 0; i < nums.size(); i++) {
         cout << nums[i];
-        if(i < nums.size - 1;) {
+        if(i < (nums.size() - 1)) {
             cout << ", ";
         }
     }
-    cout << "} "
+    cout << "} ";
+}
+// testing!!!!!!!!!!
+int main() {
+    vector<int> test = {1,1,2,2};
+    cout << "The vector is ";
+    printVector(test);
+    cout << "\nIt has " << numberOfDuplicates(test) << " duplicate(s)\n";
+
+    return 0;
 }
