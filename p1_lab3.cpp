cf99d91b9cfe1c400fa6fc0c9031d562cc68cc71
maevedoa <mlittle7823@floridapoly.edu>
2025-09-15 19:03:51 +0000

    sjdfklkds

--- p1_lab3.cpp
+++ p1_lab3.cpp
@@ -19,7 +19,7 @@ int numberOfDuplicates(const vector<int>& nums) {
 }
 
 void printVector(const vector<int>& nums) {
-    cout << "{ ";
+    cout << "{";
     for(size_t i = 0; i < nums.size(); i++) {
         cout << nums[i];
         if(i < (nums.size() - 1)) {
@@ -27,13 +27,4 @@ void printVector(const vector<int>& nums) {
         }
     }
     cout << "} ";
-}
-// testing!!!!!!!!!!
-int main() {
-    vector<int> test = {1,1,2,2};
-    cout << "The vector is ";
-    printVector(test);
-    cout << "\nIt has " << numberOfDuplicates(test) << " duplicate(s)\n";
-
-    return 0;
 }
