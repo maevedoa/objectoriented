80de495a31f758fa9e423a7c17a130353acd027d
maevedoa <mlittle7823@floridapoly.edu>
2025-09-15 18:46:25 +0000

    test

--- p2_lab3.cpp
+++ p2_lab3.cpp
@@ -6,3 +6,15 @@ using namespace std;
 void setDeepValue(int ***p, int value) {
     ***p = value;
 }
+
+int main() {
+    int x = 0;
+    int *p1 = &x;
+    int **p2 = &p1;
+    int ***p3 = &p2;
+
+    setDeepValue(p3, 42);
+    cout << "x is now " << x << "\n";
+
+return 0;
+}
